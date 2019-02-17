bea1b7af253c302048e9bdc15fb53298d6e7891e
Jona Schlegel <jona.schlegel@gmail.com>
2019-02-17 17:03:05 +0100

    Add keypress handler to search on enter

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,12 +2,9 @@ import React, { Component } from 'react';
 import './SearchBar.css';
 
 class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      term: ''
-    };
-  }
+  state = {
+    term: ''
+  };
   search = () => {
     this.props.onSearch(this.state.term);
   };
@@ -16,12 +13,18 @@ class SearchBar extends Component {
       term: event.target.value
     });
   };
+  handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      this.search();
+    }
+  };
   render() {
     return (
       <div className="SearchBar">
         <input
           placeholder="Enter A Song, Album, or Artist"
           onChange={this.handleTermChange}
+          onKeyPress={this.handleKeyPress}
         />
         <a onClick={this.search}>SEARCH</a>
       </div>
