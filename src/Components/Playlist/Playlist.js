1c2f049c4cc0f7309424a2c20c3d320a7c7b0a85
Jona Schlegel <jona.schlegel@gmail.com>
2019-02-17 17:59:10 +0100

    Add keypress handler to playlist name on enter

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -6,10 +6,19 @@ class Playlist extends Component {
   handleNameChange = event => {
     this.props.onNameChange(event.target.value);
   };
+  handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      this.props.onSave();
+    }
+  };
   render() {
     return (
       <div className="Playlist">
-        <input placeholder={'New Playlist'} onChange={this.handleNameChange} />
+        <input
+          placeholder={'New Playlist'}
+          onChange={this.handleNameChange}
+          onKeyPress={this.handleKeyPress}
+        />
         <TrackList
           tracks={this.props.playlistTracks}
           onRemove={this.props.onRemove}
